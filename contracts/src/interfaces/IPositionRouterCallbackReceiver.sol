a7b7baaa6bf19b413437b014b15609587e361d6a
Nissan Hanina <1410797+nissoh@users.noreply.github.com>
2023-05-20 21:00:54 +0300

    remove inline submodule

--- contracts/src/interfaces/IPositionRouterCallbackReceiver.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
-
-interface IPositionRouterCallbackReceiver {
-
-    function gmxPositionCallback(bytes32 positionKey, bool isExecuted, bool isIncrease) external;
-}
