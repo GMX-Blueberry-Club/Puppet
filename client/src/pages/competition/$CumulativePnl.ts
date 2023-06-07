19bb8a33ac55727658afe8668c6a297ccdf3de77
Nissan Hanina <1410797+nissoh@users.noreply.github.com>
2023-06-07 21:19:40 +0300

    update submodule

--- client/src/pages/competition/$CumulativePnl.ts
+++ client/src/pages/competition/$CumulativePnl.ts
@@ -3,7 +3,7 @@ import { $element, $node, $text, attr, component, style } from "@aelea/dom"
 import { Route } from '@aelea/router'
 import { $column, $row, layoutSheet, screenUtils } from '@aelea/ui-components'
 import { colorAlpha, pallete } from '@aelea/ui-components-theme'
-import { BLUEBERRY_REFFERAL_CODE, COMPETITION_METRIC_LIST, IBlueberryLadder, IRequestCompetitionLadderApi, TOURNAMENT_DURATION, TOURNAMENT_NEXT, TOURNAMENT_START, TOURNAMENT_TIME_ELAPSED, blueberrySubgraph } from '@gambitdao/gbc-middleware/.dist/types'
+import { BLUEBERRY_REFFERAL_CODE, COMPETITION_METRIC_LIST, IBlueberryLadder, IRequestCompetitionLadderApi, TOURNAMENT_DURATION, TOURNAMENT_NEXT, TOURNAMENT_START, TOURNAMENT_TIME_ELAPSED, blueberrySubgraph } from '@gambitdao/gbc-middleware'
 import { formatReadableUSD, formatToBasis, readableNumber } from "gmx-middleware-utils"
 import { $Link, $anchor, $infoLabeledValue, $infoTooltipLabel, ISortBy, invertColor } from '@gambitdao/ui-components'
 import { awaitPromises, empty, map, mergeArray, now, snapshot } from '@most/core'
--- middleware/src/label.ts
+++
@@ -1,9 +0,0 @@
-import { SaleType, TraitAppearanceValue } from "./types.js"
-
-export const attributeIndexToLabel: TraitAppearanceValue[] = ['Background', 'Clothes', 'Body', 'Expression', 'Face Accessory', 'Hat', 'Badge']
-
-export const mintLabelMap = {
-  [SaleType.Public]: 'Public',
-  [SaleType.holder]: 'Holder',
-  [SaleType.Whitelist]: 'Whitelist',
-}
