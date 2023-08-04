b46283604a02ec2a67e7d7a22ec6ea27d468b197
Nissan Hanina <1410797+nissoh@users.noreply.github.com>
2023-08-04 19:09:28 +0300

    tst

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,6 +1,10 @@
 
 // import 'construct-style-sheets-polyfill'
-export * from './assignThemeSync' // apply synchnously theme before all styles are being evaluated
+export * as ww from './assignThemeSync' // apply synchnously theme before all styles are being evaluated
+
+console.log(ww)
+
+
 import { runBrowser } from '@aelea/dom'
 import { $Main } from './pages/$Main'
 
