8478057a756a8305a1d776f1d0a9477685803638
Nissan Hanina <1410797+nissoh@users.noreply.github.com>
2023-08-05 23:22:51 +0300

    sw

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,11 +1,43 @@
 import { theme } from './assignThemeSync' // apply synchnously theme before all styles are being evaluated
 import { registerSW } from 'virtual:pwa-register'
+// import Wroker from './workers/background?worker'
+
+
+// replaced dyanmicaly
+const reloadSW: any = '__RELOAD_SW__'
+
+const register = registerSW({
+  onNeedRefresh() {
+    if (reloadSW === 'true') {
+      setInterval(async () => {
+        console.log('Checking for sw update')
+        await register(true)
+      }, 20000 /* 20s for testing purposes */)
+    }
+  },
+  immediate: true,
+  onRegisteredSW(swUrl, r) {
+    // eslint-disable-next-line no-console
+    console.log(`Service Worker at: ${swUrl}`)
+ 
+  },
+})
+
+// const worker = new Wroker()
 
 console.log(theme)
 
 import { runBrowser } from '@aelea/dom'
 import { $Main } from './pages/$Main'
 
+
+const date = '__DATE__'
+
+// worker.addEventListener('message', async (msg) => {
+  
+// })
+
+console.log(date)
 
 const updateSW = registerSW({
   onNeedRefresh() {
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
+import { ManifestOptions, VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
+import replace from '@rollup/plugin-replace'
+
 import { dark } from './src/common/theme'
 
 
@@ -17,45 +19,69 @@ const prefixedParentEnv = Object.fromEntries(
   Object.entries(SITE_CONFIG).map(([key, value]) => [`import.meta.env.${key}`, JSON.stringify(value)])
 )
 
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  registerType: 'prompt',
+  strategies: 'injectManifest',
+  srcDir: 'src',
+  filename: 'sw.ts',
+
+  includeAssets: ['pwa-192x192.png', 'pwa-512x512.png', 'metamask-fox.svg'],
+  manifest: {
+    name: SITE_CONFIG.APP_NAME,
+    short_name: SITE_CONFIG.APP_NAME,
+    description: SITE_CONFIG.APP_DESC_LONG,
+    theme_color: SITE_CONFIG.THEME_PRIMARY,
+    background_color: SITE_CONFIG.THEME_BACKGROUND,
+    icons: [
+      {
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+    ]
+  },
+
+  mode: 'development',
+  base: '/',
+  devOptions: {
+    enabled: process.env.SW_DEV === 'true',
+    /* when using generateSW the PWA plugin will switch to classic */
+    type: 'module',
+    navigateFallback: 'index.html',
+    suppressWarnings: true,
+  },
+}
+
+const replaceOptions = { __DATE__: new Date().toISOString() }
+const reload = process.env.RELOAD_SW === 'true'
+const selfDestroying = process.env.SW_DESTROY === 'true'
+
+
+
+if (reload) {
+  // @ts-expect-error overrides
+  replaceOptions.__RELOAD_SW__ = 'true'
+}
+
+if (selfDestroying)
+  pwaOptions.selfDestroying = selfDestroying
+
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   define: { ...prefixedParentEnv },
   publicDir: 'assets',
   plugins: [
-    VitePWA({
-      registerType: 'prompt',
-      devOptions: {
-        enabled: true
-        /* other options */
-      },
-
-      strategies: 'injectManifest',
-      srcDir: 'src',
-      filename: 'sw.ts',
-
-      includeAssets: ['pwa-192x192.png', 'pwa-512x512.png', 'metamask-fox.svg'],
-      manifest: {
-        name: SITE_CONFIG.APP_NAME,
-        short_name: SITE_CONFIG.APP_NAME,
-        description: SITE_CONFIG.APP_DESC_LONG,
-        theme_color: SITE_CONFIG.THEME_PRIMARY,
-        background_color: SITE_CONFIG.THEME_BACKGROUND,
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+    VitePWA(pwaOptions),
+    replace(replaceOptions),
   ],
-
   build: {
     outDir: ".dist",
     target: "es2022",
