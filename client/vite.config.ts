6e333c6f83dd83ad10f1eea255e12ee297ddad8b
Nissan Hanina <1410797+nissoh@users.noreply.github.com>
2023-08-08 02:16:02 +0300

    maskable icon

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -47,7 +47,13 @@ const pwaOptions: Partial<VitePWAOptions> = {
         src: 'pwa-512x512.png',
         sizes: '512x512',
         type: 'image/png'
-      }
+      },
+      {
+        src: 'pwa-maskable-560x560.png',
+        sizes: '560x560',
+        type: 'image/png',
+        purpose: "any maskable"
+      },
     ]
   },
 
