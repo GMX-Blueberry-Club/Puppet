4c68e2697f766f7b893caa4f14ac58a85da94aeb
Nissan Hanina <1410797+nissoh@users.noreply.github.com>
2024-01-17 13:34:37 +0200

    test

--- og-middleware.ts
+++ og-middleware.ts
@@ -1,16 +1,15 @@
+import type { Context } from "@netlify/edge-functions"
 
-export default async function handler(request, context) {
+export default async function handler(request: Request, context: Context) {
   const response = await context.next()
   const page = await response.text()
-    
-
   const url = new URL(request.url)
   const pathParam = url.searchParams.get('path') || ''
   const titleParam = url.searchParams.get('title') || ''
   const cacheParam = url.searchParams.get('cache') || '86400'
 
-  const replacement = `$1og-screenast/?path=${encodeURIComponent(pathParam)}&title=${encodeURIComponent(titleParam)}&cache=${encodeURIComponent(cacheParam)}$2`
-  const replacedPage = page.replace(/(<meta property="og:image" content=")[^"]*(">)/, replacement)
+  const replacement = `og-screenast/?path=${encodeURIComponent(pathParam)}&title=${encodeURIComponent(titleParam)}&cache=${encodeURIComponent(cacheParam)}`
+  const replacedPage = page.replace(/(<meta property="og:image" content=")[^"]*(">)/, `$1${replacement}$2`)
 
   response.headers.set('cache-control', `s-maxage=${cacheParam}`)
 
